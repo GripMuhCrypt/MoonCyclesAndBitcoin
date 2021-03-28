545e4b7674b0183f0fcf4fdcebfde8fe63940330
GripMuhCrypt <gripmuhcrypt@protonmail.com>
2021-03-28 09:08:00 -0700

    Initial commit

--- BitcoinMoon.py
+++ BitcoinMoon.py
@@ -11,9 +11,10 @@ import requests
 ##locs, labels = xticks()  
 
 #######################################Collect lunar variables
-Year = int (input("type year start"))
-Month = int(input("what month homie?"))
-Day = int(input("It's the first of the month aka DAY plz sir"))
+Year = 2013
+Month = 1
+Day = 1
+Ticker =  input( "Type eth or btc to chart in log form: ")
 ###################################################Calculate data from inputs
 #A=Y/100
 A = Year/100
@@ -37,12 +38,22 @@ CurrentDaysIntoCycle = (NewMoonsSince%1)*29.53
 #print(CurrentDaysIntoCycle)
 ###########################################################################################################
 # get info
-Request = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max')
+if Ticker == 'btc':
+   btc = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max')
+   Dictionary = json.loads(btc.text)
+   arrayData = Dictionary['prices']
+elif Ticker == 'eth':
+   eth = requests.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=max')
+   Dictionary = json.loads(eth.text)
+   arrayData = Dictionary['prices']
+else:
+   print("Program error")
+   quit()
 ###################################SORT JSON
 #convert from to dict type
-Dictionary = json.loads(Request.text)
+#Dictionary = json.loads(Request.text)
 #convert data down to array
-arrayData = Dictionary['prices']
+
 #get array info
 MAX = len(arrayData)
 MIN = 0
@@ -60,14 +71,11 @@ y1 = [0]*MAX
 y2 = np.array(y1)
 indexY = 0
 MA = [0]*MAX
-#fill array with values for Y @ X
+#fill array with values for Y 
 while ( indexY < MAX):
-   y2[ indexY ] = arrayData[ indexY ][1]
-   if indexY >= 1:
-      average = (y2[ indexY ]+y2[ indexY-1 ])/indexY
-   if indexY >= 200 :
-      MA[indexY] = 
-   indexY += 1
+   y2[ indexY ] = arrayData[ indexY ][1] 
+   indexY += 1 
+
 #################
 
 ###########################################################################################################
@@ -85,22 +93,6 @@ x = 0
 while (y < int(DaysSinceNew )): #actual Y variable
     Yarr[y] = ((y/29.53)%1)*29.5
     y += 1
-'''
-###########################################################################Plot data 1
-plt.plot(np.arange(0, int(DaysSinceNew)),Yarr[0:int(DaysSinceNew)] )
-#xticks(np.arange(0, DaysSinceNew, step=5))
-plt.xlabel("days")
-plt.ylabel("moon phase")
-plt.title('Moon Cycles')
-plt.show()
-###########################################################################Plot data 2
-plt.plot(np.arange(0,MAX), y2)
-plt.xticks(np.arange(0, MAX, step=1))
-plt.xlabel("days")
-plt.ylabel("price")
-plt.title('BTC')
-plt.show()
-'''
 #########################New Plot
 ##REference:https://www.geeksforgeeks.org/matplotlib-pyplot-twinx-in-python/
 fig, ax1 = plt.subplots()
@@ -118,13 +110,5 @@ ax2.set_ylabel('btc', color = green)
 ax2.plot( x2, y2, color = green)
 ax2.set_yscale('log')
 ax2.tick_params(axis = 'y', labelcolor = green)
-
-ax3 = ax1.twinx()
-
-red = 'tab:red'
-ax3.set_ylabel('200 MA', color = red)
-ax3.plot( x2, MA, color = red)
-ax3.set_yscale('log')
-ax3.tick_params(axis = 'y', labelcolor = red)
 
 plt.show()
